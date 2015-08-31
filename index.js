1afe7e5af494f57fed5e3636e52a655b3ec97ee6
Sebastian Blask <blask@gmx.de>
2015-08-31 15:35:17 +0200

    Use page-mod instead of custom stuff

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const core = require("sdk/view/core");
 const file = require("sdk/io/file");
+const pageMod = require("sdk/page-mod");
 const preferences = require("sdk/preferences/service");
 const self = require("sdk/self");
 const simplePreferences = require('sdk/simple-prefs');
-const tabs = require("sdk/tabs");
 
 const NewTabURL = require('resource:///modules/NewTabURL.jsm').NewTabURL;
 
@@ -21,39 +21,6 @@ function clearUrlBar(tab) {
     urlBar.focus();
 }
 
-function attachFiles(tab) {
-     let worker = tab.attach({
-        contentScriptFile: [
-            "./jquery-2.1.4.js",
-            "./bookmark_dial.js",
-        ],
-    });
-    workerRegistry.register(tab, worker);
-    updateStyle();
-}
-
-function maybeLoadBookmarDial(tab) {
-    if (tab.url == constants.URL) {
-        tab.on("load", clearUrlBar);
-        tab.on("activate", clearUrlBar);
-        tab.on("load", bookmarks.update);
-        tab.on("close", workerRegistry.deregister);
-        attachFiles(tab);
-    }
-}
-
-function arePropertiesReliable(tab) {
-    return tab.readyState !== "uninitialized" && tab.readyState !== "loading";
-}
-
-function onTabOpen(tab) {
-    if (arePropertiesReliable(tab)) {
-        maybeLoadBookmarDial(tab);
-    } else {
-        tab.on("ready", maybeLoadBookmarDial);
-    }
-}
-
 function updateDial() {
     let folder = simplePreferences.prefs.bookmarkFolder;
     if (folder) {
@@ -62,29 +29,57 @@ function updateDial() {
 }
 
 function __getStyleString() {
+    //  cannot use href for local file as file:// does not work,
+    //  so load file directly using the appropriate functions
+    let useCustomStyleFile = simplePreferences.prefs.useCustomStyleFile;
     let customStyleFile = simplePreferences.prefs.customStyleFile;
-    if (simplePreferences.prefs.useCustomStyleFile &&
-        customStyleFile && file.exists(customStyleFile)) {
-        return file.read(simplePreferences.prefs.customStyleFile);
+    if (useCustomStyleFile && customStyleFile && file.exists(customStyleFile)) {
+        return file.read(customStyleFile);
     } else {
         return self.data.load("./bookmark_dial.css");
     }
 }
 
-function updateStyle() {
-    //  cannot use href for local file as file:// does not work,
-    //  so load file directly using the appropriate functions
-    workerRegistry.message("styleUpdate", __getStyleString());
+function updateStyle(worker) {
+    let message = "styleUpdate";
+    let data = __getStyleString();
+    if (worker) {
+        worker.port.emit(message, data);
+    } else {
+        workerRegistry.message(message, data);
+    }
 }
 
 // hijack homepage and new tab page
 preferences.set("browser.startup.homepage", "about:newtab");
 NewTabURL.override(constants.URL);
+
 // setup listeners
 bookmarks.on("update", ui.bookmarkTreeToContextMenu);
 bookmarks.on("update", updateDial);
 simplePreferences.on("bookmarkFolder", updateDial);
-simplePreferences.on("customStyleFile", updateStyle);
-simplePreferences.on("useCustomStyleFile", updateStyle);
-// init for the right tabs
-tabs.on("open", onTabOpen);
+simplePreferences.on("customStyleFile", function() {updateStyle();});
+simplePreferences.on("useCustomStyleFile", function() {updateStyle();});
+
+pageMod.PageMod({
+    include: constants.URL,
+    attachTo: ["existing", "top"],
+    contentScriptFile: [
+        "./jquery-2.1.4.js",
+        "./bookmark_dial.js",
+    ],
+    onAttach: function(worker) {
+        console.log("Attach");
+        worker.on('detach', function () {
+            console.log("Detach");
+            workerRegistry.deregister(this);
+        });
+        workerRegistry.register(worker);
+        clearUrlBar(worker.tab);
+        worker.tab.on("activate", clearUrlBar);
+        worker.tab.on("pageshow", clearUrlBar);
+        updateStyle(worker);
+        bookmarks.update();
+    }
+});
+
