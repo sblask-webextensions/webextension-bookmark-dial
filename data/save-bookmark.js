b35cb366829900870aa6b22a0784cf17a0cbe9c4
Sebastian Blask <blask@gmx.de>
2015-10-02 18:46:22 +0200

    Send form data from panel

--- data/save-bookmark.js
+++ data/save-bookmark.js
@@ -1,15 +1,24 @@
 /* global addon */
 
 function initFormHandler() {
+    let form = document.querySelector("form");
+
+    function extractData() {
+        let data = {};
+        for (let formElement of form.elements) {
+            if (formElement.type != "submit") {
+                data[formElement.name] = formElement.value || undefined;
+            }
+        }
+        return data;
+    }
+
     function onSubmit(event) {
         event.preventDefault();
-        let data = {};
-        // TODO
-        addon.port.emit("save", data);
+        addon.port.emit("save", extractData());
         return false;
     }
 
-    let form = document.querySelector("form");
     form.addEventListener("submit", onSubmit, false);
 }
 
