3c34a50858f993947c6fe886385c2bbfa92f1df4
Sebastian Blask <blask@gmx.de>
2017-08-02 11:37:05 +0200

    Add options skeleton

---
+++ webextension/options/options.js
@@ -0,0 +1,40 @@
+function restoreOptions() {
+    browser.storage.local.get([
+    ]).then(
+        result => {
+        }
+    );
+}
+
+function enableAutosave() {
+    for (let input of document.querySelectorAll("input:not([type=radio]):not([type=checkbox]), textarea")) {
+        input.addEventListener("input", saveOptions);
+    }
+    for (let input of document.querySelectorAll("input[type=radio], input[type=checkbox]")) {
+        input.addEventListener("change", saveOptions);
+    }
+}
+
+function setTextValue(elementID, newValue) {
+    let oldValue = document.getElementById(elementID).value;
+
+    if (oldValue !== newValue) {
+        document.getElementById(elementID).value = newValue;
+    }
+}
+
+function setBooleanValue(elementID, newValue) {
+    document.getElementById(elementID).checked = newValue;
+}
+
+function saveOptions(event) {
+    event.preventDefault();
+    browser.storage.local.set({
+    });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("DOMContentLoaded", enableAutosave);
+document.querySelector("form").addEventListener("submit", saveOptions);
+
+browser.storage.onChanged.addListener(restoreOptions);
