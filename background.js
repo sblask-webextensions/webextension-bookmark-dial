a907804d8dfa43c38d8ffcc2f5277f7ddaa27b7b
Sebastian Blask <blask@gmx.de>
2017-09-12 16:36:18 +0200

    Generate thumbnail when adding or changing bookmark for the current tab

--- background.js
+++ background.js
@@ -16,6 +16,57 @@ browser.storage.local.get([
         }
     );
 
+function createThumbnail(bookmarkURL) {
+    Promise.all([
+        browser.tabs.captureVisibleTab(),
+        browser.tabs.executeScript({ code: "window.innerWidth" }),
+        browser.tabs.executeScript({ code: "window.innerHeight" }),
+        browser.tabs.query({ active: true, currentWindow: true }).then(tabs => tabs[0].url),
+    ])
+        .then(result => { return dataURLToCanvas(...flatten(result), bookmarkURL); })
+        .then(canvas => { console.log(canvas.toDataURL()); });
+}
+
+function flatten(list) {
+    return [].concat.apply([], list);
+}
+
+function dataURLToCanvas(dataURL, originalWidth, originalHeight, currentTabURL, bookmarkURL) {
+    if (currentTabURL !== bookmarkURL) {
+        return null;
+    }
+
+    let [newWidth, newHeight] = getNewSizing(originalWidth, originalHeight);
+
+    return new Promise(
+        function(resolve, _reject) {
+            let canvas = document.createElement("canvas");
+            canvas.width = newWidth;
+            canvas.height = newHeight;
+
+            let context = canvas.getContext("2d");
+
+            let image = new Image();
+            image.onload = function() {
+                context.drawImage(image, 0, 0, newWidth, newHeight, 0, 0, newWidth, newHeight);
+                return resolve(canvas);
+            };
+            image.src = dataURL;
+        }
+    );
+}
+
+function getNewSizing(originalWidth, originalHeight) {
+    if (originalWidth / originalHeight > 1.5) {
+        return [originalWidth / 3 * 2, originalHeight];
+    } else {
+        return [originalWidth, originalHeight / 3 * 2];
+    }
+}
+
+browser.bookmarks.onCreated.addListener((_id, bookmark) => createThumbnail(bookmark.url));
+browser.bookmarks.onChanged.addListener((_id, changeInfo) => createThumbnail(changeInfo.url));
+
 function handleInstalled(details) {
     if (details.reason === "update" && details.previousVersion === "1.1.2") {
         browser.tabs.create({
