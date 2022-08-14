f51b1eb1db09f3da3a035a6be1e4e72a0787e672
Tiago Rascazzi <tiago@rascazzi.com>
2022-08-14 16:55:21 -0400

    Clean

--- background.js
+++ background.js
@@ -82,18 +82,6 @@ function createThumbnail(bookmarkURL) {
     ]);
 }
 
-function delay(t, v) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve.bind(null, v), t)
-    });
- }
-
- Promise.prototype.delay = function(t) {
-     return this.then(function(v) {
-         return delay(t, v);
-     });
- }
-
 
 function loadThumbnail(bookmarkURL, imageData) {
     var img = new Image();
